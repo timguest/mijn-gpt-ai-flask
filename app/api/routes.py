de78faac9d0d894cadd0383f6466eee1ab9f7da4
JBLDSKY <87128575+JBLDKY@users.noreply.github.com>
2023-11-15 23:20:29 +0100

    cors

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,7 +3,10 @@ from flask_cors import CORS
 from ..scraper.scraper import Scraper
 from ..core.chatapi import generate_response, test_response
 
+load_dotenv()
+
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": ALLOW_LIST}})
 
 
 @app.route("/message", methods=["POST"])
@@ -12,12 +15,13 @@ async def receive_message():
     message_data = request.json
     message_text = message_data["text"]
 
-    answer = await generate_response(message_text)
+    # res = await generate_response(message_text)
     # answer = await test_response(message_text)
+    res = "Hey, the endpoint is working anyway ;) "
 
     # "message" value is displayed as the AI's response in the frontend
     return (
-        jsonify({"status": "success", "message": f"dit is het antwoord {answer}"}),
+        jsonify({"status": "success", "message": res}),
         200,
     )
 
@@ -33,6 +37,3 @@ def receive_url():
 
     # "message" value is displayed as the AI's response in the frontend
     return jsonify({"status": "success", "message": f"Message received: {url}"}), 200
-
-
-CORS(app)
